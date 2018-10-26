f518931289fa5a66335289b878fc0e3c7295d6b6
eldelcesar <cesarjr.espinosa@gmail.com>
2018-10-25 19:14:51 -0500

    Test completo y comentarios de problema

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -50,7 +50,22 @@ object Parser {
     val compoundStmtRef: BaseParser<Any?> = ref { compoundStmt }
     val localDeclarationsRef: BaseParser<Any?> = ref { localDeclarations }
     val paramListRef: BaseParser<Any?> = ref { paramList }
-
+    val declarationListRef: BaseParser<Any?> = ref { declarationList }
+    val declarationRef: BaseParser<Any?> = ref { declaration }
+    val funDeclarationRef: BaseParser<Any?> = ref { funDeclaration }
+    val varDeclarationRef: BaseParser<Any?> = ref { varDeclaration }
+    val statementRef: BaseParser<Any?> = ref { statement }
+    val expressionStmtRef: BaseParser<Any?> = ref { expressionStmt }
+    val selectionSmtRef: BaseParser<Any?> = ref { selectionSmt }
+    val iterationSmtRef: BaseParser<Any?> = ref { iterationSmt }
+    val typeSpecifierRef: BaseParser<Any?> = ref { typeSpecifier }
+    val callRef: BaseParser<String> = ref { call }
+    val paramsRef: BaseParser<Any?> = ref { params }
+    val paramRef: BaseParser<Any?> = ref { param }
+    val variableRef: BaseParser<String> = ref { variable }
+    val simpleExpressionRef: BaseParser<String?> = ref { simpleExpression }
+    val factorRef: BaseParser<String> = ref { factor }
+    val argsRef: BaseParser<String?> = ref { args }
 
 
     // Operators
@@ -73,9 +88,9 @@ object Parser {
         } ?: throw IllegalStateException()
     })
 
-    val args = argList
+    val args = argListRef
 
-    val call = id and (leftParen then args before rightParen) map {
+    val call = (id before (leftParen before rightParen) map { it.raw }) or (id and (leftParen then argsRef before rightParen) map {
         exp -> val (a, b) = exp
         new_Temp()
         val temp = tempStack.pop()
@@ -85,7 +100,7 @@ object Parser {
                 "$temp = $b\n" +
                 "param $temp\n" +
                 "$temp2 = call ${a.raw}, 1"
-    }
+    })
 
     val variable: BaseParser<String> = (id and (leftBracket then expressionRef before rightBracket) map {
         exp -> val (a, b) = exp
@@ -96,18 +111,18 @@ object Parser {
             val temp1 = tempStack.pop()
             val temp2 = tempStack.pop()
             val temp3 = tempStack.pop()
-           "\n$temp3 = sizeof ${a.raw}\n" +
-                "$temp2 = $b * $temp3\n" +
-                "$temp1 = &${a.raw} + $temp1"
+            "\n$temp3 = sizeof ${a.raw}\n" +
+                    "$temp2 = $b * $temp3\n" +
+                    "$temp1 = &${a.raw} + $temp1"
         } ?: throw IllegalStateException()
     }) or (id map { it.raw })
 
-    val factor: BaseParser<String> = (number map { it.raw }) or call or variable or ((leftParen then expressionRef before rightParen) map {
+    val factor: BaseParser<String> = callRef or variableRef or (number map { it.raw }) or ((leftParen then expressionRef before rightParen) map {
         exp -> val a = exp
         a?.let { it } ?: throw IllegalStateException()
     })
 
-    val term: BaseParser<String> = ((factor and mulop) and termRef map {
+    val term: BaseParser<String> = ((factorRef and mulop) and termRef map {
         exp -> val (a, b) = exp
         new_Temp()
         when (a.second.type) {
@@ -123,7 +138,7 @@ object Parser {
         }
     }) or factor
 
-    val additiveExpression = ((term and addop) and additiveExpressionRef map {
+    val additiveExpression = ((termRef and addop) and additiveExpressionRef map {
         exp -> val (a, b) = exp
         new_Temp()
         when(a.second.type) {
@@ -139,7 +154,7 @@ object Parser {
         }
     }) or term
 
-    val simpleExpression = (additiveExpression and relop and additiveExpression) map {
+    val simpleExpression = (additiveExpressionRef and relop and additiveExpressionRef) map {
         exp -> val (a, b) = exp
         new_Temp()
         new_Temp()
@@ -171,35 +186,42 @@ object Parser {
             }
             else -> throw IllegalStateException()
         }
-    } or additiveExpression
+    } or additiveExpressionRef
 
-    val expression: BaseParser<String> = ((variable and assign map { it }) and expressionRef map {
+    val expression: BaseParser<String?> = ((variableRef and assign map { it }) and expressionRef map {
         exp -> val (a, b) = exp
         "${a.first} ${a.second.raw} $b"
-    }) or simpleExpression
+    }) or simpleExpressionRef
 
     val expressionStmt =  expressionRef before semicolon or semicolon
 
-    val param = typeSpecifier then id
+    val param = typeSpecifierRef then id
 
-    val paramList =  param map { "" } or (paramListRef before comma and param map {""})
+    val paramList =  (paramRef before comma and paramListRef map {""}) or paramRef map { "" }
 
-    val params = paramList or void_
+    val params = paramListRef or void_ map { it.toString() }
 
-    val varDeclaration = typeSpecifier then id before semicolon map { "" }
+    val varDeclaration = typeSpecifierRef then id before semicolon map { "" }
 
-    val localDeclarations = varDeclaration or (varDeclaration and localDeclarationsRef map { "" })
+    val localDeclarations = (varDeclarationRef and localDeclarationsRef) or varDeclarationRef
 
-    val compoundStmt = leftBrace then localDeclarations then statementListRef before rightBrace map {
+    val compoundStmt = (leftBrace then rightBrace) or (leftBrace then localDeclarationsRef then statementListRef before rightBrace map {
         exp -> val a = exp
-        a?.let {
-            a.toString() }
-    }
+        a?.let { a.toString() }
+    }) or (leftBrace then localDeclarationsRef before rightBrace map {
+        exp -> val a = exp
+        a?.let { a.toString() }
+    }) or (leftBrace then statementListRef before rightBrace map {
+        exp -> val a = exp
+        a?.let { a.toString() }
+    })
 
-    val funDeclaration = typeSpecifier then id and leftParen then params then rightParen and compoundStmt map {
+    val returnSmt = (return_ before semicolon map { it.raw }) or (return_ and expressionRef before semicolon map { "${it.first.raw} ${it.second}" })
+
+    val funDeclaration = typeSpecifierRef then id and (leftParen then paramsRef before rightParen) and compoundStmt map {
         exp -> val (a, b) = exp
         b?.let {
-            "entry ${a.raw}\n$b"
+            "\nentry ${a.first.raw}\n$b"
         } ?: throw IllegalStateException()
     }
 
@@ -223,43 +245,36 @@ object Parser {
         a.first?.let {
             val temp = tempStack.pop()
             val label = tempLabel.pop()
-            "$temp = ${a.first}\nif false $temp goto $label\n${a.second}\n" +
+            "$temp = ${a.first}\n" +
+                "if false $temp goto $label\n${a.second}\n" +
                 "Label $label\n$b"
         } ?: throw IllegalStateException()
     }) or ((if_ then leftParen then expressionRef before rightParen) and statementListRef map{
         exp -> val (a, b) = exp
+        new_Temp()
+        new_Label()
         a?.let {
-            "t1 = $a\n if false t1 goto L1\n$b"
+            val temp = tempStack.pop()
+            val label = tempLabel.pop()
+            "$temp = $a\n" +
+                "if false $temp goto $label\n$b\n" +
+                "Label $label\n"
         } ?: throw IllegalStateException()
     })
 
-    val statement = (expressionStmt or compoundStmt or selectionSmt or iterationSmt or varDeclaration) map { it.toString() }
+    val statement = (expressionStmtRef or compoundStmtRef or selectionSmtRef or iterationSmtRef or varDeclarationRef or returnSmt) map { it.toString() }
 
-    val statementList = (statement and statementListRef map {
+    val statementList = (statementRef and statementListRef map {
         exp -> val (a, b) = exp
         a?.let {
             b?.let { "$a\n$b " }
         } ?: throw IllegalStateException()
-    }) or statement
+    }) or statementRef
 
-    /*
-    La gramatica en realidad deberia ser:
 
-    val declaration = funDeclaration or varDeclaration
-    val declarationList = declarationListRef and declaration or declaration
+    val declaration = funDeclarationRef or varDeclarationRef
+    val declarationList = (declaration and declarationListRef) or declaration
     val program = declarationList
-
-    Tiene el statementList como un caso base para que podamos probar
-    las demas funcionalidades.
-
-    El problema que tenemos es con funDeclaration y declarationList
-    porque al parecer no encuentran un caso base y terminan el proceso
-    */
-    // val declaration =  funDeclaration or varDeclaration
-
-    val program = statementList or varDeclaration
-
-
 
     // Support functions
     fun getParser(): BaseParser<Any?> { return program }
@@ -275,70 +290,4 @@ object Parser {
         this.countLabel++
         return "L$countLabel"
     }
-
-
-
-
-
-
-    // Recursive Rules
-    /*val declarationListRef: BaseParser<TokenInstance> = ref { declarationList }
-    val additiveExpressionRef: BaseParser<TokenInstance> = ref { additiveExpression }
-    val paramListRef: BaseParser<TokenInstance> = ref { paramList }
-    val localDeclarationsRef: BaseParser<TokenInstance?> = ref { localDeclarations }
-    val statementListRef: BaseParser<TokenInstance?> = ref { statementList }
-    val compoundStmtRef: BaseParser<TokenInstance> = ref { compoundStmt }
-    val statementRef: BaseParser<TokenInstance> = ref { statement }
-    val expressionRef: BaseParser<TokenInstance> = ref { expression }
-    val termRef: BaseParser<TokenInstance> = ref { term }
-    val variableRef: BaseParser<TokenInstance> = ref { variable }
-    val argListRef: BaseParser<TokenInstance> = ref { argList }*/
-
-
-    // Special characters Rules
-    /*
-    val relop = lessEqual or less or more or moreEqual or equal or notEqual
-    val addop = plus or minus
-    val mulop = times or divide
-    val typeSpecifier = int_ or void_
-    */
-
-
-    // General Rules
-    /*
-    val argList = (argListRef then comma then expressionRef) or expressionRef
-    val args = argList or empty<TokenInstance>()
-    val call = id then leftParen then args then rightParen
-    val factor = (leftParen then expressionRef then rightParen) or
-                                         variableRef or call or number
-    val term = (termRef then mulop then factor) or factor
-    val additiveExpression = (additiveExpressionRef then addop then term) or
-                             term
-    val simpleExpression = (additiveExpression then relop then additiveExpression) or
-                                                   (additiveExpression)
-    val variable = id or (id then leftBracket then expressionRef then rightBracket)
-    val expression = (variable then assign then expressionRef) or
-                     (simpleExpression)
-    val returnSmt = (return_ then semicolon) or
-                                            (return_ then expression)
-    val iterationSmt = while_ then leftParen then expression then statementRef
-    val selectionSmt = (if_ then leftParen then expression then rightParen then statementRef) or
-                       (if_ then leftParen then expression then rightParen then statementRef then else_ then statementRef)
-    val expressionSmt = (expression then semicolon) or semicolon
-    val statement = expressionSmt or compoundStmtRef or selectionSmt or iterationSmt or returnSmt
-    val statementList = (statementListRef then statement) or empty<TokenInstance>()
-    val varDeclaration = (typeSpecifier then id then semicolon) or
-                                                 (typeSpecifier then id then leftBracket then number then rightParen then semicolon)
-    val localDeclarations = (localDeclarationsRef then varDeclaration) or empty<TokenInstance>()
-    val compoundStmt = leftBrace then localDeclarations map {} then statementList before rightBrace
-    val param = (typeSpecifier then id) or
-                                         (typeSpecifier then id then leftBracket then rightParen)
-    val paramList = (paramListRef then comma then param) or
-                                            param
-    val params = paramList or void_
-    val funDeclaration = typeSpecifier then id then leftParen then params then rightParen then compoundStmt
-    val declaration = varDeclaration or funDeclaration
-    val declarationList = ( declarationListRef then declaration) or declaration
-    val program = declarationList
-    */
 }
